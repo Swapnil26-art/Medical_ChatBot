c19961681bd3e7f8ed5b1eb97c2376c478939d6a
swapnil26-art <sp2120@srmist.edu.in>
2025-04-02 22:36:09 +0530

    folder structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Swapnil Panda',
+    author_email= 'sp2120@srmist.edu.in',
+    packages= find_packages(),
+    install_requires = []
+
+)
