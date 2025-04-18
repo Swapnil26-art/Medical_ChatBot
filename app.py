55d7367aaa09eaa97e6b111991589141fb77c9c7
swapnil26-art <sp2120@srmist.edu.in>
2025-04-18 19:36:15 +0530

    project completion 80 percent

---
+++ app.py
@@ -0,0 +1,85 @@
+from flask import Flask, render_template, jsonify, request
+from src.helper import download_hugging_face_embeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain_openai import AzureChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from dotenv import load_dotenv
+import os
+
+app = Flask(__name__)
+
+load_dotenv()
+
+PINECONE_API_KEY= ''
+OPENAI_API_KEY=os.environ.get('AZURE_OPENAI_API_KEY')
+
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+os.environ["AZURE_OPENAI_API_KEY"] = OPENAI_API_KEY
+
+embeddings = download_hugging_face_embeddings()
+
+
+index_name = "medicalbot"
+
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+
+
+#Extract Data From the PDF File
+def load_pdf_file(data):
+    loader= DirectoryLoader(data,
+                            glob="*.pdf",
+                            loader_cls=PyPDFLoader)
+
+    documents=loader.load()
+
+    return documents
+
+
+docsearch = PineconeVectorStore.from_existing_index(
+    index_name=index_name,
+    embedding=embeddings
+)
+
+retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":2})
+
+
+llm = AzureChatOpenAI(
+    deployment_name="chatengienee",  # your Azure model deployment name
+    api_key=os.environ["AZURE_OPENAI_API_KEY"],
+    azure_endpoint=os.environ["AZURE_ENDPOINT"],
+    api_version="2024-02-01",
+    temperature=0.4,
+    max_tokens=500
+)
+prompt_template = ChatPromptTemplate.from_messages([
+    ("system", "You are an assistant for medical questions. Use the context to answer accurately. Be brief.\n\n{context}"),
+    ("human", "{input}")
+])
+
+question_answer_chain = create_stuff_documents_chain(llm, prompt_template)
+
+rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+
+
+@app.route("/")
+def index():
+    return render_template('chat.html')
+
+
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    print(input)
+    response = rag_chain.invoke({"input": msg})
+    print("Response : ", response["answer"])
+    return str(response["answer"])
+
+
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=8080, debug=True)  # binds to localhost only
